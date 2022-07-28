2ec817b16aba8244f3036507390e886b071105a9
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-07-28 15:05:00 +0100

    Create analysis.sh

---
+++ analysis.sh
@@ -0,0 +1,20 @@
+#!Usr/bin/bash
+
+#Importing numpy for  
+import numpy as np
+
+#Importing pandas for 
+import pandas as pd
+
+#Import matplotlib.pyplot for 
+import matplotlib.pyplot as plt
+
+#Import seaborn for 
+import seaborn as sns
+
+#Importing covid-19 vaccination dataset 
+data = pd.read_csv("country_vaccinations.csv")
+
+#Exploring and viewing data 
+data.head()
+data.describe()
